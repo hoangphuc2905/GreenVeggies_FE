4d34adf8b335480a28ae49dd21cb7f15c0c28e0e
Phan Hoang Tan <phanhoangtan.job@gmail.com>
2025-02-02 13:00:07 +0700

    build Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import logo from "./assets/Green.png";
+
+import Home from "./pages/Home";
+
 
 
 export default function App() {
   return (
-    // <h1 className="text-3xl font-bold underline">
-    //   {/* Website GreenVeggies welcomes you! */}
-    // </h1>
-    <div className="flex justify-center items-center h-screen">
-      <img src={logo} alt="logo" className="h-96 w-96" />
-    </div>
+    <>
+    <Home />
+    </>
+
+  
   );
 }
