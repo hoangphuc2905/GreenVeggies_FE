b48774d5ceac995cb014251cd031c4f64b518909
Pkkoi1 <khoinhokboddy@gmail.com>
2025-02-12 20:35:09 +0700

    Profile detail, products

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Main from './pages/Admin/main/main'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
 )
