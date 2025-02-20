56693cd0f89e125bc6aff1170088a68b78a13fec
minhthuan <121850122+ThuanMinh29@users.noreply.github.com>
2025-02-20 17:37:00 +0700

    them menu va giao dien

--- src/pages/user/Profile/userprofile.jsx
+++ src/pages/user/Profile/userprofile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function ProfileForm() {
     const [profile, setProfile] = useState({
--- src/routes/UserRouter.jsx
+++ src/routes/UserRouter.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/user/Home";
 import Product from "../pages/user/Products/page";
 import Detail from "../pages/user/Products/detail";
+import Profile from "../pages/user/Profile/page";
 
 const UserRouter = () => {
   return (
@@ -9,6 +10,7 @@ const UserRouter = () => {
       <Route path="/" element={<Home />} />
       <Route path="/product" element={<Product />} />
       <Route path="/product/:id" element={<Detail />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
