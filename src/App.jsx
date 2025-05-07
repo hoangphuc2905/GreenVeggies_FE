3c2768df86ee7d35100b830f21a35620439bc07e
Pkkoi1 <khoinhokboddy@gmail.com>
2025-05-07 12:08:07 +0700

    refactor: remove RoleRedirect component and related role handling from App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,11 @@ import UserRouter from "./routes/UserRouter";
 import { Provider } from "react-redux";
 import { store } from "./redux/store"; // Import store
 import ErrorRouter from "./routes/ErrorRouter";
-import RoleRedirect from "./utils/RoleRedirect"; // Import RoleRedirect
 
 const App = () => {
-  const role = "guest";
-
   return (
     <Provider store={store}>
       <Router>
-        {/* <RoleRedirect role={role} /> Add RoleRedirect */}
         <Routes>
           <Route path="/admin/*" element={<AdminRouter />} />
           <Route path="/*" element={<UserRouter />} />
